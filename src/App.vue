67709f6645846006249222a61bab7ea858818247
you@example.com <rudabzerin@gmail.com>
2021-10-07 11:22:15 +0600

    thirc commit

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,23 @@
 <template>
   <div>
      <h1> Welcome</h1>
-     <newprac class="bazz boo"> </newprac>
+     
      <!-- <signup> </signup>   -->
      <!-- <modal> </modal> -->
-     <br>
-     <pracarr> </pracarr>
+     <student> </student>
      
-     <br>
-     <todotask> </todotask>
+
   </div>
 </template>
 
 <script>
-// import Modal from './components/modal.vue'
-// import Signup from './components/signup.vue'
-import Newprac from './components/newprac.vue'
-import Todotask from './components/TodoTask.vue'
-import PracArr from './components/pracarr.vue'
+
+import Student from './components/student.vue'
 export default {
   name: 'App',
   components: {
-    
-    // Signup,
-    Newprac,
-    Todotask,
-    PracArr
+
+    Student
   },
 }
 </script>
---
+++ src/components/student.vue
@@ -0,0 +1,69 @@
+<template>
+  <div>
+    <form v-on:submit.prevent="onSubmit">
+      <input type="text" label="name" v-model="name" />
+      <input type="number" label="roll" v-model="rollNo" />
+      <input type="text" label="fathers name" v-model="FathersName" />
+      <input type="text" label="address" v-model="address" />
+      <button>submit</button>
+    </form>
+
+    <table>
+      <thead>
+        <tr>
+          <th>name</th>
+          <th>roll</th>
+          <th>action</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>miraz</td>
+          <td>100</td>
+          <td>
+
+            <button @click="ChangeBtn"> {{ button.text }}</button>
+            <button @click = 'delArr(index)'>delete</button>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+    <br />
+  </div>
+</template>
+
+
+<script>
+export default {
+    data(){
+   return {
+       
+        studentObject:  
+        {
+              name: '',
+              rollNo: '',
+              FathersName: '',
+              address: '',
+        },
+        students_details:[],
+        button:{
+          text: 'Edit'
+        },
+        edit: true,
+    }
+  },
+  methods: {
+    onSubmit(){
+      return this.students_details.push(this.studentObject);
+    },
+    changeBtn(){
+      student.edit = !student.edit;
+      student.button.text = student.edit ? 'Edit' : 'Update'
+    },
+    delArr(index){
+      this.students_details.splice(index, 1);
+    }
+    
+  }
+}
+</script>
